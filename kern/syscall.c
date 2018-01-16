c9093124f011d3845c56978ce67c4405b3abe7ca
jia <jia@example.com>
2018-01-16 17:20:20 +0800

    lab6 commit

--- kern/syscall.c
+++ kern/syscall.c
@@ -12,6 +12,7 @@
 #include <kern/console.h>
 #include <kern/sched.h>
 #include <kern/time.h>
+#include <kern/e1000.h>
 
 // Print a string to the system console.
 // The string is exactly 'len' characters long.
@@ -414,7 +415,24 @@ static int
 sys_time_msec(void)
 {
 	// LAB 6: Your code here.
-	panic("sys_time_msec not implemented");
+	// panic("sys_time_msec not implemented");
+	return time_msec();
+}
+
+// Send network packet
+static int
+sys_netpacket_try_send(void *addr, size_t len)
+{
+	user_mem_assert(curenv, addr, len, PTE_U);
+	return e1000_transmit(addr, len);
+}
+
+// Receive network packet
+static int
+sys_netpacket_recv(void *addr, size_t buflen)
+{
+	user_mem_assert(curenv, addr, buflen, PTE_U | PTE_W);
+	return e1000_receive(addr, buflen);
 }
 
 // Dispatches to the correct kernel function, passing the arguments.
@@ -458,6 +476,14 @@ syscall(uint32_t syscallno, uint32_t a1, uint32_t a2, uint32_t a3, uint32_t a4, 
 			return sys_ipc_recv((void *)a1);
 		case SYS_env_set_priority:
 			return sys_env_set_priority(a1, a2);
+		case SYS_env_set_trapframe:
+			return sys_env_set_trapframe(a1, (struct Trapframe*)a2);
+		case SYS_time_msec:
+			return sys_time_msec();
+		case SYS_netpacket_try_send:
+			return sys_netpacket_try_send((void *)a1, (size_t)a2);
+		case SYS_netpacket_recv:
+			return sys_netpacket_recv((void *)a1, (size_t)a2);
 		default:
 			return -E_NO_SYS;
 	}
